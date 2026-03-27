9bf6ddabe5d2e977204a9aaf0002c0dd00029aee
DKooPK <dkoopk.god@gmail.com>
2026-03-27 14:12:30 +0300

    created site

---
+++ about.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Дмитрий Яковлев | Главная</title>
+</head>
+<body>
+    
+    <?require("require/header.php")?>
+
+    <div class="content p-5 bg-light-subtle">
+
+        <div class="ad d-flex flex-column flex-md-row m-4 justify-content-sm-between align-items-center bg-dark-subtle">
+
+            <div class="flexpart image">
+                <a data-fslightbox href="assets/image/Dmitry.jpg">
+                    <img src="assets/image/Dmitry.jpg" class="img-fluid">
+                </a>
+            </div>
+            
+            <div class="flexpart text">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    Ratione tempora modi consectetur sint repellat magnam accusamus error vel asperiores autem? 
+                    Accusamus pariatur maiores nobis excepturi repudiandae esse odit, at officia qui quia recusandae officiis 
+                    possimus omnis molestiae voluptate. Aperiam, a exercitationem. Quia ab suscipit inventore nostrum nesciunt, 
+                    corrupti ullam nobis!
+                </p>
+            </div>
+
+        </div>
+
+        <div class="ad d-flex flex-column flex-md-row m-4 justify-content-sm-between align-items-center bg-light-subtle">
+
+            <div class="flexpart text">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione tempora modi consectetur sint repellat magnam 
+                    accusamus error vel asperiores autem? Accusamus pariatur maiores nobis excepturi repudiandae esse odit, at officia qui quia 
+                    recusandae officiis possimus omnis molestiae voluptate. Aperiam, a exercitationem. Quia ab suscipit inventore nostrum nesciunt, 
+                    corrupti ullam nobis!
+                </p>
+            </div>
+
+            <div class="flexpart image">
+                <a data-fslightbox href="assets/image/Dmitry.jpg">
+                    <img src="assets/image/Dmitry.jpg" class="img-fluid">
+                </a>
+            </div>
+
+        </div>
+
+        <div class="ad d-flex flex-column flex-md-row m-4 justify-content-sm-between align-items-center bg-dark-subtle">
+
+            <div class="flexpart image">
+                <a data-fslightbox href="assets/image/Dmitry.jpg">
+                    <img src="assets/image/Dmitry.jpg" class="img-fluid">
+                </a>
+            </div>
+            
+            <div class="flexpart text">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    Ratione tempora modi consectetur sint repellat magnam accusamus error vel asperiores autem? 
+                    Accusamus pariatur maiores nobis excepturi repudiandae esse odit, at officia qui quia recusandae officiis possimus 
+                    omnis molestiae voluptate. Aperiam, a exercitationem. Quia ab suscipit inventore nostrum nesciunt, corrupti ullam nobis!
+                </p>
+            </div>
+
+        </div>
+
+
+    </div>
+
+     <?require("require/footer.php")?>
+    
+</body>
+</html>
---
+++ arts.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Дмитрий Яковлев | Главная</title>
+</head>
+<body>
+    
+    <?require("require/header.php")?>
+
+    <div class="content p-5 bg-light-subtle">
+
+        <h1 class="text-center">Мои работы</h1>
+
+        <div class="art m-4 d-flex justify-content-sm-between justify-content-center flex-wrap align-items-center">
+
+            <div class="card bg-dark-subtle" style="width: 17rem;">
+                    <a data-fslightbox href="assets/image/tempImage.jpg">
+                        <img src="assets\image\tempImage.jpg" class="card-img-top">
+                    </a>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Card title</h5>
+                </div>
+            </div>
+
+            <div class="card bg-dark-subtle" style="width: 17rem;">
+                    <a data-fslightbox href="assets/image/tempImage.jpg">
+                        <img src="assets\image\tempImage.jpg" class="card-img-top">
+                    </a>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Card title</h5>
+                </div>
+            </div>
+    
+            <div class="card bg-dark-subtle" style="width: 17rem;">
+                    <a data-fslightbox href="assets/image/tempImage.jpg">
+                        <img src="assets\image\tempImage.jpg" class="card-img-top">
+                    </a>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Card title</h5>
+                </div>
+            </div>
+
+            <div class="card bg-dark-subtle" style="width: 17rem;">
+                    <a data-fslightbox href="assets/image/tempImage.jpg">
+                        <img src="assets\image\tempImage.jpg" class="card-img-top">
+                    </a>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Card title</h5>
+                </div>
+            </div>
+
+            <div class="card bg-dark-subtle" style="width: 17rem;">
+                    <a data-fslightbox href="assets/image/tempImage.jpg">
+                        <img src="assets\image\tempImage.jpg" class="card-img-top">
+                    </a>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Card title</h5>
+                </div>
+            </div>
+
+            <div class="card bg-dark-subtle" style="width: 17rem;">
+                    <a data-fslightbox href="assets/image/tempImage.jpg">
+                        <img src="assets\image\tempImage.jpg" class="card-img-top">
+                    </a>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Card title</h5>
+                </div>
+            </div>
+
+            <div class="card bg-dark-subtle" style="width: 17rem;">
+                    <a data-fslightbox href="assets/image/tempImage.jpg">
+                        <img src="assets\image\tempImage.jpg" class="card-img-top">
+                    </a>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Card title</h5>
+                </div>
+            </div>
+
+            <div class="card bg-dark-subtle" style="width: 17rem;">
+                    <a data-fslightbox href="assets/image/tempImage.jpg">
+                        <img src="assets\image\tempImage.jpg" class="card-img-top">
+                    </a>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Card title</h5>
+                </div>
+            </div>
+
+            <div class="card bg-dark-subtle" style="width: 17rem;">
+                    <a data-fslightbox href="assets/image/tempImage.jpg">
+                        <img src="assets\image\tempImage.jpg" class="card-img-top">
+                    </a>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Card title</h5>
+                </div>
+            </div>
+
+            <div class="card bg-dark-subtle" style="width: 17rem;">
+                    <a data-fslightbox href="assets/image/tempImage.jpg">
+                        <img src="assets\image\tempImage.jpg" class="card-img-top">
+                    </a>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Card title</h5>
+                </div>
+            </div>
+            
+        </div>
+
+    </div>
+
+     <?require("require/footer.php")?>
+    
+</body>
+</html>
